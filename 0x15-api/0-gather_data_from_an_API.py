bb3335009448054de44a0cf02e0bbfd83238bbd6
Introvertedtechie <zillionaire234@gmail.com>
2023-04-24 15:02:21 +0000

    £$€¥₦

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
 """
-    Gather data from an API and display TODO list progress for a given employee ID
+Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
 """
-
-import sys
 import requests
+import sys
+
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2 and sys.argv[1].isdigit():
-        user_id = int(sys.argv[1])
-        base_url = "https://jsonplaceholder.typicode.com"
-        user_res = requests.get(base_url + "/users/{}".format(user_id))
-        todo_res = requests.get(base_url + "/todos?userId={}".format(user_id))
-
-        try:
-            user_data = user_res.json()
-            todo_data = todo_res.json()
-
-            total_tasks = len(todo_data)
-            completed_tasks = len([task for task in todo_data if task.get("completed")])
-
-            print("Employee {} is done with tasks({}/{}):".format(user_data.get("name"),
-                                                                    completed_tasks,
-                                                                    total_tasks))
-
-            for task in todo_data:
-                if task.get("completed"):
-                    print("\t {}".format(task.get("title")))
-        except ValueError:
-            print("Error: Invalid JSON format")
-    else:
-        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+    user_id = sys.argv[1]
+    user_name = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                             .format(user_id)).json().get('name')
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                        .format(user_id)).json()
+    done_tasks = [task for task in todo if task.get('completed') is True]
+    total_tasks = len(todo)
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user_name, len(done_tasks), total_tasks))
+    for task in done_tasks:
+        print('\t', task.get('title'))
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
 """
-Gather data from an API and export TODO list progress for a given employee ID in CSV format
+Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
 """
-
 import csv
-import sys
 import requests
+import sys
+
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2 and sys.argv[1].isdigit():
-        user_id = int(sys.argv[1])
-        base_url = "https://jsonplaceholder.typicode.com"
-        user_res = requests.get(base_url + "/users/{}".format(user_id))
-        todo_res = requests.get(base_url + "/todos?userId={}".format(user_id))
+    user_id = sys.argv[1]
+    user_name = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                             .format(user_id)).json().get('username')
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                        .format(user_id)).json()
 
-        try:
-            user_data = user_res.json()
-            todo_data = todo_res.json()
-
-            csv_file = "{}.csv".format(user_id)
-            with open(csv_file, mode='w') as file:
-                writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                for task in todo_data:
-                    writer.writerow([user_id, user_data['username'], task['completed'], task['title']])
-        except ValueError:
-            print("Error: Invalid JSON format")
-    else:
-        print("Usage: ./1-export_to_CSV.py <employee_id>")  
+    with open(user_id + ".csv", mode='w') as file:
+        writer = csv.writer(file, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL)
+        for task in todo:
+            writer.writerow([user_id, user_name, task.get('completed'),
+                             task.get('title')])
