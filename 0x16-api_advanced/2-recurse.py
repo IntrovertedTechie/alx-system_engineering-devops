46a2cc5b3f20517019d86251949102811b1b1743
Introvertedtechie <zillionaire234@gmail.com>
2023-05-09 22:21:09 +0000

    api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit.
+Queries the API and returns a list containing the titles of all hot articles.
 """
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """Returns a list containing the titles of all hot articles for a given subreddit."""
+    """Returns a list containing the titles of all hot articles."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     params = {'after': after} if after else None
