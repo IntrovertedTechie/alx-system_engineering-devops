5effd774ab9a153ee9197588241ff895073a12b9
Introvertedtechie <zillionaire234@gmail.com>
2023-05-09 17:42:49 +0000

    api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,17 +5,14 @@ This module contains a function that queries the Reddit API.
 import requests
 
 
-
 def top_ten(subreddit):
     """Print the titles of the first 10 hot posts for a given subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
         print("None")
         return
     data = response.json()
-    for post in data['data']['children']:
-        print(post['data']['title'])
-
+    for post in data['data']['children']:        print(post['data']['title'])
