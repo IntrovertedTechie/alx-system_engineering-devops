95e8d963606201a645d067d51c9c2ef663ac2c18
Introvertedtechie <zillionaire234@gmail.com>
2023-05-09 16:43:53 +0000

    ap

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
-"""This module contains a function that queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit."""
+"""This module contains a function that queries the Reddit API.
+"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts for a given subreddit."""
+    """Print the titles of the first 10 hot posts for a given subreddit.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers)
@@ -16,4 +18,3 @@ def top_ten(subreddit):
     data = response.json()
     for post in data['data']['children']:
         print(post['data']['title'])
-
