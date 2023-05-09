581dca61bc89cc058bc2200deebe4ec3260a31fb
Introvertedtechie <zillionaire234@gmail.com>
2023-05-09 22:52:43 +0000

    api

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,36 +1,43 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-Queries the Reddit API and counts the occurrences of each keyword.
+Queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit.
 """
-
 import requests
 
 
-def recurse(subreddit, word_list, count_dict={}, after=None):
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+def count_words(subreddit, word_list, word_counts={}):
+    """Queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords"""
+    hot_list = recurse(subreddit, [])
+    if hot_list:
+        for title in hot_list:
+            words = title.lower().split()
+            for word in words:
+                if word.rstrip('.,?!') in word_list:
+                    word_counts[word.rstrip('.,?!')] = word_counts.get(word.rstrip('.,?!'), 0) + 1
+        sorted_word_counts = sorted(word_counts.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_word_counts:
+            print("{}: {}".format(word, count))
+    else:
+        print("No posts match or subreddit is invalid.")
+
+
+def recurse(subreddit, hot_list=[], after="", word_counts={}):
+    """Returns a list containing the titles of all hot articles for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit, after)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'after': after} if after else None
-    response = requests.get(url, headers=headers,
-                            params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json().get('data')
         if data:
             children = data.get('children')
             if children:
                 for post in children:
-                    title = post['data']['title'].lower()
-                    for word in word_list:
-                        if word.lower() in title and not any(c.isalpha() for c in title[title.index(word.lower()) - 1]):
-                            count_dict[word.lower()] = count_dict.get(
-                                word.lower(), 0) + title.count(word.lower())
+                    hot_list.append(post['data']['title'])
                 after = data.get('after')
                 if after:
-                    return recurse(subreddit, word_list, count_dict, after)
+                    return recurse(subreddit, hot_list, after, word_counts)
                 else:
-                    sorted_counts = sorted(
-                        count_dict.items(), key=lambda x: (-x[1], x[0]))
-                    for word, count in sorted_counts:
-                        print("{}: {}".format(word, count))
+                    return hot_list
             else:
                 return None
         else:
