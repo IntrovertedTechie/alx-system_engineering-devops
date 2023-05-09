747945e41c918aaa556dd64d90a997c1d678389f
Introvertedtechie <zillionaire234@gmail.com>
2023-05-09 22:39:51 +0000

    api

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,36 +1,39 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Queries the Reddit API and counts the occurrences of each keyword.
+"""
+
 import requests
 
 
-def count_words(subreddit, word_list, after='', counter={}):
-    """Prints a sorted count of given keywords."""
+def recurse(subreddit, word_list, count_dict={}, after=None):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'limit': 100, 'after': after}
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-
-    if response.status_code != 200:
-        return
-
-    data = response.json()
-    posts = data.get('data', {}).get('children', [])
-    for post in posts:
-        title = post.get('data', {}).get('title', '').lower()
-        for word in word_list:
-            word = word.lower()
-            if word in title:
-                if word in counter:
-                    counter[word] += 1
+    params = {'after': after} if after else None
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json().get('data')
+        if data:
+            children = data.get('children')
+            if children:
+                for post in children:
+                    title = post['data']['title'].lower()
+                    for word in word_list:
+                        if word.lower() in title and not any(c.isalpha() for c in title[title.index(word.lower()) - 1]):
+                            count_dict[word.lower()] = count_dict.get(
+                                word.lower(), 0) + title.count(word.lower())
+                after = data.get('after')
+                if after:
+                    return recurse(subreddit, word_list, count_dict, after)
                 else:
-                    counter[word] = 1
-
-    after = data.get('data', {}).get('after', '')
-    if after is None:
-        sorted_words = sorted(counter.items(),
-                              key=lambda x: (-x[1], x[0]))
-        for word, count in sorted_words:
-            print("{}: {}".format(word, count))
-        return
-
-    return count_words(subreddit, word_list, after, counter)
+                    sorted_counts = sorted(
+                        count_dict.items(), key=lambda x: (-x[1], x[0]))
+                    for word, count in sorted_counts:
+                        print("{}: {}".format(word, count))
+            else:
+                return None
+        else:
+            return None
+    else:
+        return None
